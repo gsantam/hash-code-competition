17ce6c57212693ddc69891b39ed95f9cfecdec27
Guillermo Santamaria <santamaria.guille@gmail.com>
2019-01-13 18:14:08 +0100

    Main

--- 2017/main.py
+++ 2017/main.py
@@ -1,4 +1,9 @@
 from utils.input_parser import InputParser
+from utils.output_parser import OutputParser
+
 
 input_parser = InputParser('inputs/example.in')
 input_parser.parse()
+
+output_parser = OutputParser(input_parser,'inputs/example.out')
+output_parser.parse()
